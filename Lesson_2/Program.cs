b70ca1db0ca3d2b0b3c9e1cdd67b6098029b7c10
bdotgeorge <bzzz2009@yandex.ru>
2023-02-14 18:11:14 +0400

    faster solution task 13

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,4 +1,5 @@
-﻿void TaskTen()
+﻿using System.Diagnostics;
+void TaskTen()
 {
     Console.Write("The program takes a three-digit number as input and displays the second digit of this number as output\nEnter an integer from 100 to 999\n");
     int number = 0;
@@ -11,12 +12,25 @@ void TaskThirteen()
     int number = 0;
     Console.WriteLine((int.TryParse(s, out number) && Math.Abs(number) > 99) ? $"{Math.Abs(number).ToString()[2]}" : "No third digit");
 }
+void TaskThirteenAnotherSolution() //faster than TaskThirteen
+{
+    Console.Write("The program which prints the third digit of the given number or reports that there is no third digit\n");
+    string s = Console.ReadLine()!;
+    int number = 0;
+    int.TryParse(s, out number);
+    number = Math.Abs(number);
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine((number > 99) ? $"{s} -> {number % 10}" : $"{s} -> No third digit");
+}
 void TaskFifteen()
 {
     Console.Write("The program which takes as input a digit indicating the day of the week and checks if this day is a weekend\nEnter an integer from 1 to 7\n");
     int number = 0;
     int.TryParse(Console.ReadLine()!, out number);
-    if(number < 1 || number > 7)
+    if (number < 1 || number > 7)
         Console.Write("Not a Day of Week");
     else
         Console.WriteLine((number > 5) ? $"{number} -> Yes" : $"{number} -> No");
@@ -25,3 +39,4 @@ void TaskFifteen()
 TaskTen();
 TaskThirteen();
 TaskFifteen();
+TaskThirteenAnotherSolution();
