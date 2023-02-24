855c31460b35bc5ada06a89937e7ef067b7a9b85
bdotgeorge <bzzz2009@yandex.ru>
2023-02-24 13:22:38 +0400

    Lesson Four complite

--- Lesson_4/Lesson.cs
+++ Lesson_4/Lesson.cs
@@ -7,18 +7,59 @@ public class Lesson
     {
         data = value;
     }*/
-    public int TakeConsoleInt(string str = "Enter integer"){
+    
+
+    public void PrintArray(Array massive){
+        int size = massive.Length;
+        for(int i = 0; i < size; i++){
+            Console.Write($"Print Array data {i} = {massive.GetValue(i)}\n");
+        }
+    }
+    public int[] TaskTwentyNineCreateArray(int sizeArray = 8){
+        int[] tempArray = new int[sizeArray];
+        return tempArray;
+    }
+    public void fillArray(ref int[] array){
+        int size = array.Length;
+        for(int i = 0; i < size; i++){
+            array[i] = new Random().Next(0, size);
+        }
+    }
+    public void EnterTaskNumber(){
+        int taskNumber = TakeConsoleInt("Enter task number");
+        switch (taskNumber)
+        {
+            case 25:
+                Console.WriteLine("The program takes two numbers (A and B) as input and raises the number A to the natural power of B.");
+                Console.Write($"Result work task 25 = {TaskTwentyFiveDegreeOf(TakeConsoleInt("Enter A number"), TakeConsoleInt("Enter B degree"))}\n");
+                break;
+            case 27:
+                Console.WriteLine("The program takes a number as input and returns the sum of the digits in the number.");
+                Console.Write($"Result work task 27 = {TaskTwentySevenSumNumInteger(TakeConsoleInt())}\n");
+                break;
+            case 29:
+                Console.WriteLine("The program sets an array of 8 elements and displays them on the screen");
+                int[] mas = TaskTwentyNineCreateArray();
+                fillArray(ref mas);
+                PrintArray(mas);
+                break;
+            default:
+                Console.Write($"Wrong task number \n");
+                break;
+        }
+    }
+    private int TakeConsoleInt(string str = "Enter integer"){
         Console.Write($"{str}\n");
         return StrToInt(Console.ReadLine()!);
     }
 
-    public int StrToInt(string str){
+    private int StrToInt(string str){
         int num = 0;
         int.TryParse(str, out num);
         return num;
     }
 
-    public double DegreeOf(int number, int degree){
+    private double TaskTwentyFiveDegreeOf(int number, int degree){
         double result = number;
         if(degree > 0){
             for (int i = 1; i < degree; i++) {
@@ -33,7 +74,7 @@ public class Lesson
         return result;
     }
 
-    public int SumNumInteger(int number){
+    private int TaskTwentySevenSumNumInteger(int number){
         number = Math.Abs(number);
         string str = number.ToString();
         int size = str.Length;
@@ -46,21 +87,5 @@ public class Lesson
         return sum;
     }
 
-    public int[] CreateArray(int sizeArray = 8){
-        int[] tempArray = new int[sizeArray];
-        return tempArray;
-    }
-    public void fillArray(ref int[] array){
-        int size = array.Length;
-        for(int i = 0; i < size; i++){
-            array[i] = new Random().Next(0, 15);
-        }
-    }
-
-    public void PrintArray(Array massive){
-        int size = massive.Length;
-        for(int i = 0; i < size; i++){
-            Console.Write($"{massive.GetValue(i)}\n");
-        }
-    }
+    
 }
--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -1,65 +1,10 @@
 ﻿using LessonFour;
 
-int TakeConsoleInt(string str = "Enter integer"){
-    Console.Write($"{str}\n");
-    return StrToInt(Console.ReadLine()!);
-}
-
-int StrToInt(string str){
-    int num = 0;
-    int.TryParse(str, out num);
-    return num;
-}
-
-double DegreeOf(int number, int degree){
-    double result = number;
-    if(degree > 0){
-        for (int i = 1; i < degree; i++) {
-            result *= number;
-        }
-    }
-    else{
-        for (int i = degree; i <= 0; i++) {
-            result /= number;
-        }
-    }
-    return result;
-}
-
-int SumNumInteger(int number){
-    number = Math.Abs(number);
-    string str = number.ToString();
-    int size = str.Length;
-    int sum = number % 10;
-    for (int i = 0; i < size; i++)
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        number /= 10;
-        sum += number % 10;
-    }
-    return sum;
-}
-
-int[] CreateArray(int sizeArray = 8){
-    int[] tempArray = new int[sizeArray];
-    return tempArray;
-}
-void fillArray(ref int[] array){
-    int size = array.Length;
-    for(int i = 0; i < size; i++){
-        array[i] = new Random().Next(0, 15);
+        Lesson lessonFour = new Lesson();
+        lessonFour.EnterTaskNumber();
     }
 }
-
-void PrintArray(Array massive){
-    int size = massive.Length;
-    for(int i = 0; i < size; i++){
-        Console.Write($"{massive.GetValue(i)}\n");
-    }
-}
-int[] r = CreateArray();
-fillArray(ref r);
-PrintArray(r);
-Lesson lessonFour = new Lesson();
-//lessonFour.
-
-//Console.Write($"{SumNumInteger(-9012)}\n");
