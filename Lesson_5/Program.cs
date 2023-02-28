fbfb9ae9aa35ce8415225bd7dce85d3733e23895
bdotgeorge <bzzz2009@yandex.ru>
2023-02-28 17:41:05 +0400

    lesson 5

--- Lesson_5/Program.cs
+++ Lesson_5/Program.cs
@@ -5,5 +5,8 @@ internal class Program
     {
         LessonFive lesson = new LessonFive();
         lesson.EnterTaskNumber();
+        int a = 13;
+        string str = Convert.ToString(a, 2);
+        //text.Split(' ').Where(i => int.TryParse(i, out _)).Select(int.Parse).ToArray();
     }
 }
